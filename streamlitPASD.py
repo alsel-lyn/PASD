d811d84a93f0ba1eccfc7fef446990cbe733863e
alsel-lyn <136293949+alsel-lyn@users.noreply.github.com>
2023-06-12 12:13:58 +0700

    Update streamlitPASD.py

--- streamlitPASD.py
+++ streamlitPASD.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import seaborn as sb
 
 #Header
-st.title("ANALYZING DATA : LAKU DAN TIDAK LAKU")
+st.title("ANALYZING DATA : LAKU ATAU TIDAK LAKU")
 st.header("Mata kuliah : Perancangan Aplikasi Sains Data")
 st.write(" 1. Alya Selynindya (1305210079) \n 2. Shamaya Mayra Argyanti(1305213112) \n 3. Jati Tepatasa Bagastakwa(1305213059)" )
 st.text(" - Aplikasi sains data untuk memprediksi laku dan tidak laku") 
