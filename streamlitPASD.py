11ce4f114f281e7c535c695c5ec666e32cfd89cc
alsel-lyn <136293949+alsel-lyn@users.noreply.github.com>
2023-06-12 11:20:51 +0700

    Update streamlitPASD.py

--- streamlitPASD.py
+++ streamlitPASD.py
@@ -2,6 +2,8 @@ import streamlit as st
 import random as rd
 import numpy as np
 import pandas as pd
+import matplotlib
+matplotlib.use('Agg')  # Set the backend to Agg
 import matplotlib.pyplot as plt
 import seaborn as sb
 
